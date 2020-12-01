40e1ee5581bfa172defc655e075e1682588d1875
treasure-praise <treasurepraiseolatunbosun@yahoo.com>
2020-12-01 02:57:57 -0800

    created a search box form and separated the seach box into it's individual component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
-// import logo from './logo.svg';
-// import './App.css';
+import {SearchBox} from "./components/search-box/search-box.component"
+import "./App.css"
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchField:""
     };
   }
 
@@ -18,9 +20,16 @@ class App extends Component {
   }
 
   render() {
+    
+const {monsters,searchField}=this.state;
+const filteredMonsters = monsters.filter(monster=>
+    monster.name.toLowerCase().includes(searchField.toLowerCase() )
+  )
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <SearchBox placeholder='search monsters'handleChange={e=>this.setState({searchField:e.target.value})} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
